676d9d83e0a9a934fc1e896d0a33d4e7f1b194ff
patrick38894 <snixromero@gmail.com>
2013-07-25 17:37:22 -0700

    hexagonal graph linking (buggy)
    still quite buggy

--- Hexagon.cs
+++ Hexagon.cs
@@ -1,8 +1,15 @@
-class Hexagon {
+using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+class Hexagon : MonoBehaviour {
 	public Triangle [] triangles = new Triangle[6];
-	public static float diamond = 1f / 2f * Math.Sqrt(3);
-	public static float hourglass = 2f / math.Sqrt(3);
-	public static float diagonal = 1.0f;
+	public Hexagon [] hexagons = new Hexagon[6];
+	public static float diamond = 1f / 2f * (float) Math.Sqrt(3);
+	public static float hourglass = 2f / (float) Math.Sqrt(3);
+	public static float sideLength = 1.0f;
+	public static float sqrt3 = (float) Math.Sqrt(3);
+	protected static bool begun = false;
 	
 	public int clock6(int toClock) {
 		if (toClock >= 0)
@@ -10,36 +17,92 @@ class Hexagon {
 		return 6 + toClock; //only works for numbers >= -6
 	}
 
-	public int clock12(int toClock) {
-		if (toClock >= 0)
-			return toClock % 12;
-		return toClock + 12; // for n >= -12
-	}
-	
-	public int clock12Invert(int toInvert) {
-		return clock12(toInvert + 6);
-	}
-
 	public int clock6Invert(int toInvert) {
-		return clock12(toInvert + 3);
+		return clock6(toInvert + 3);
 	}
 
 	public void linkTo(int index, Hexagon targetHex) {
-		Triangle.doubleLink(triangles[index], 2 * index, targetHex.triangles[clock6Invert(index)], clock12Invert(2 * index), diamond); //mid2mid
-		Triangle.doubleLink(triangles[clock6(index-1)], clock12(2 * index + 1),
-						targetHex.triangles[clock6Invert(index)], clock12(clock12Invert(2 * index) + 1), diagonal); //left2mid
-		Triangle.doubleLink(triangles[clock6(index+1)], clock12(2 * index - 1),
-						targetHex.triangles[clock6Invert(index)], clock12(clock12Invert(2 * index) - 1), diagonal); //right2mid
-		Triangle.doubleLink(triangles[index], clock12(2 * index-1),
-						targetHex.triangles[clock6(clock6invert(index) +1)], clock12(clock12invert(index * 2) - 1), diagonal); //mid2left
-		Triangle.doubleLink(triangles[index], clock12(2 * index+1),
-						targetHex.triangles[clock6(clock6invert(index) -1)], clock12(clock12invert(index * 2) + 1), diagonal); //mid2right
-		Triangle.doubleLink(triangles[clock6(index-1), clock12(index * 2),
-						targetHex.triangles[clock6(clock6invert(index) +1)], clock12(clock12invert(index * 2)), hourglass); //left2left
-		Triangle.doubleLink(triangles[clock6(index+1), clock12(index * 2),
-						targetHex.triangles[clock6(clock6invert(index) -1)], clock12(clock12invert(index * 2)), hourglass); //left2left
+		Triangle.doubleLink(triangles[index], targetHex.triangles[clock6Invert(index)], diamond); //mid2mid
+		Triangle.doubleLink(triangles[clock6(index-1)], 
+						targetHex.triangles[clock6(clock6Invert(index) +1)], hourglass); //left2left
+		Triangle.doubleLink(triangles[clock6(index+1)], 
+						targetHex.triangles[clock6(clock6Invert(index) -1)], hourglass); //left2left
 	}
 
 	public void linkIn () {
+	}
+	
+	protected static Hexagon [] hexarray;
+	public static int xdim = 8;
+	public static int ydim = 8;
+	
+	public void initWrapper() {
+		hexarray = new Hexagon[xdim];
+		hexarray[0] = GetComponent<Hexagon>();
+		init(0, 0);
+	}
+
+	public void init (int x, int y) {
+		Vector3 position = transform.position;
+		bool isEven = (y%2 ==0);
+		
+		if (isEven &&  y!=0) {//link upwards leaning to the right
+			linkTo(0,hexarray[x]);
+			if (x > 0)
+				linkTo(5,hexarray[x-1]);
+		}
+		else if (! isEven) {
+			linkTo(5,hexarray[x]);
+			if (x < xdim -1) {
+				linkTo(0,hexarray[x+1]);
+		}
+		
+		if (y == ydim-1)
+			if ((isEven && x == xdim-1) || (!isEven && x == 0))
+				return;
+		if (isEven)
+			position.x += sqrt3 * sideLength; 
+		else
+			position.x -= sqrt3 * sideLength; 
+		if (x == 0 || x == xdim-1)
+			position.x -= sideLength;
+
+		GameObject nextHex = Instantiate(this, position, Quaternion.identity) as GameObject;
+		Hexagon next = nextHex.GetComponent<Hexagon>();
+
+		if (isEven) {
+			if (x == xdim-1)
+				linkTo(2,next);
+			else
+				linkTo(1,next);
+		}
+		else {
+			if (x == 0)
+				linkTo(3,next);
+			else
+				linkTo(4,next);
+		}
+		hexarray[x] = hexagons[0];
+		if (isEven) {
+			if (x == xdim-1) {
+				next.init(x,y+1);
+				return;
+			}
+			next.init(x+1,y);
+			return;
+		}
+		if (x == 0) {
+			next.init(x,y+1);
+			return;
+		}
+		next.init(x-1,y);
+		return;
+		}
+	}
+	public void Start () {
+		if (!begun) {
+			begun = true;
+			initWrapper();
+		}
 	}
 }
--- Triangle.cs
+++ Triangle.cs
@@ -1,3 +1,28 @@
-class Triangle {
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
+class Triangle {
+	class Edge {
+		public Edge(Triangle dst, float dist){
+			dest = dst;
+			distance = dist;
+		}
+		public Triangle dest;
+		public float distance;
+	}
+
+
+	public static void doubleLink(Triangle src, Triangle dst, float dist){
+		dst.link(src, dist);
+		src.link(dst, dist);
+	}
+
+	public void link(Triangle src, float dist){
+		adjacent.Add(new Edge(src, dist));
+	}
+	Transform transform;
+	List<Edge> adjacent = new List<Edge> ();
 }
+
+
